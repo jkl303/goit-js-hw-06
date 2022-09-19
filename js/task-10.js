461cf277b46d4fb62c3891a75487733e4166ec11
jkl303 <303vladzoz@gmail.com>
2022-09-19 15:18:33 +0200

    add amount

--- js/task-10.js
+++ js/task-10.js
@@ -7,8 +7,13 @@ const destroyBtn = document.querySelector('[data-destroy]');
 const boxes = document.querySelector('#boxes');
 let boxSize = 20;
 const boxSet = new Set();
+let amount = 0;
+const onInput = () => {
+  amount = Number(input.value);
+  return amount;
+}
 const createBoxes = () => {
-  for (let i = 0; i < Number(input.value); i += 1) {
+  for (let i = 0; i < amount; i += 1) {
     boxSize += 10;
     const createdBox = document.createElement('div');
     createdBox.style.width = boxSize + 'px';
@@ -23,5 +28,6 @@ const destroyAllBoxes = () => {
   boxSet.clear();
   boxSize = 20;
 };
+input.addEventListener('input', onInput);
 createBtn.addEventListener('click', createBoxes);
 destroyBtn.addEventListener('click', destroyAllBoxes);
